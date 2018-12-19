93b997cbe703dc66441c40f97bdeca8dedd506ce
KylinDC <inlovingdays@gmail.com>
2018-12-19 23:13:59 +0800

    finish get best charge of orders

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,127 +1,35 @@
 function bestCharge(orders) {
+  let ordersInfo = getOrdersInfo(orders);
+  let result = generateResult(ordersInfo);
+  return result;
+}
 
+function getOrdersInfo(orders) {
+  let ordersInfo = {}
+  ordersInfo.orderDetailArray = getOrderDetails(orders);
+  ordersInfo.totalOriginPrice = getTotalPrice(ordersInfo.orderDetailArray);
+  ordersInfo.discount = getBiggerDiscount(ordersInfo);
+  ordersInfo.totalPrice = ordersInfo.totalOriginPrice - ordersInfo.discount.discountValue;
+  return ordersInfo;
+}
+
+function getOrderDetails(orders) {
   let orderDetailArray = [];
-
-  let totalPrice = 0;
-
   for (let order of orders) {
     orderDetailArray.push(getOrderDetail(order));
   }
-
-  for (let orderDetail of orderDetailArray) {
-    totalPrice += orderDetail.orderPrice;
-  }
-
-  let priceBreakDiscount = getPriceBreakDiscount(totalPrice, 30, 6);
-
-  let itemPercentDiscounts = { discountItems: [], discount: 0 };
-  for (let order of orders) {
-    itemPercentDiscount = getItemPercentDiscount(order, 0.5);
-    if (itemPercentDiscount) {
-      itemPercentDiscounts.discountItems.push(itemPercentDiscount.discountItem);
-      itemPercentDiscounts.discount += itemPercentDiscount.discount;
-    }
-  }
-
-  let selectedPromotion = getSelectedPromotion(priceBreakDiscount, itemPercentDiscounts);
-
-  totalPrice -= selectedPromotion.discount;
-
-  let bestChargeInfo = {}
-  bestChargeInfo.orderDetail = orderDetailArray;
-  bestChargeInfo.selectedPromotion = selectedPromotion;
-  bestChargeInfo.totalPrice = totalPrice;
-
-
-
-
-
-
-
-
-
-
-
-
-  let orderDetailMessage = "";
-
-  for (let i of bestChargeInfo.orderDetail) {
-    orderDetailMessage += `${i.name} x ${i.amount} = ${i.orderPrice}元
-`
-  }
-
-  orderDetailMessage = orderDetailMessage.trim();
-
-  let message;
-
-  if (bestChargeInfo.selectedPromotion.discount > 0) {
-
-    message = `============= 订餐明细 =============
-${orderDetailMessage}
------------------------------------
-使用优惠:
-${bestChargeInfo.selectedPromotion.promotion}，省${bestChargeInfo.selectedPromotion.discount}元
------------------------------------
-总计：${bestChargeInfo.totalPrice}元
-===================================`}
-
-
-  else {
-    message = `============= 订餐明细 =============
-${orderDetailMessage}
------------------------------------
-总计：${bestChargeInfo.totalPrice}元
-===================================`
-  }
-
-
-
-
-
-  return message;
+  return orderDetailArray;
 }
 
 function getOrderDetail(order) {
   let orderDetail = {};
   let item = getItemByID(order.split(' x ')[0]);
+  orderDetail.id = item.id;
   orderDetail.name = item.name;
+  orderDetail.itemPrice = item.price;
   orderDetail.amount = parseInt(order.split(' x ')[1]);
-  orderDetail.orderPrice = getOrderPrice(item, orderDetail.amount);
+  orderDetail.orderPrice = getOrderPrice(item.price, orderDetail.amount);
   return orderDetail;
-}
-
-function getOrderPrice(item, amount) {
-  return item.price * amount;
-}
-
-function getPriceBreakDiscount(totalPrice, price, discount) {
-  return Math.floor((totalPrice / price)) * discount;
-}
-
-function getItemPercentDiscount(order, discountPercent) {
-  let promotionItems = loadPromotions()[1]['items'];
-  let itemId = order.split(' x ')[0];
-
-  if (promotionItems.includes(itemId)) {
-    let itemPercentDiscount = {};
-    let price = getItemByID(itemId).price;
-    itemPercentDiscount.discountItem = getItemByID(itemId).name;
-    itemPercentDiscount.discount = price * discountPercent;
-    return itemPercentDiscount;
-  }
-
-}
-
-function getSelectedPromotion(priceBreakDiscount, itemPercentDiscounts) {
-  let selectedPromotion = {};
-  if (priceBreakDiscount >= itemPercentDiscounts.discount) {
-    selectedPromotion.promotion = loadPromotions()[0].type;
-    selectedPromotion.discount = priceBreakDiscount;
-  } else {
-    selectedPromotion.promotion = `${loadPromotions()[1].type}(${itemPercentDiscounts.discountItems.join('，')})`;
-    selectedPromotion.discount = itemPercentDiscounts.discount;
-  }
-  return selectedPromotion;
 }
 
 function getItemByID(id) {
@@ -131,4 +39,104 @@ function getItemByID(id) {
       return i;
     }
   }
+}
+
+function getTotalPrice(orderDetailArray) {
+  return orderDetailArray.reduce((acc, cur) => acc + cur.orderPrice, 0);
+}
+
+function getOrderPrice(price, amount) {
+  return price * amount;
+}
+
+function getBiggerDiscount(ordersInfo) {
+  let priceBreakDiscount = {};
+  priceBreakDiscount.type = loadPromotions()[0].type;
+  priceBreakDiscount.discountValue = getPriceBreakDiscountValue(ordersInfo.totalOriginPrice, 30, 6);
+
+  let itemPercentDiscounts = {};
+  itemPercentDiscounts.type = loadPromotions()[1].type;
+  [itemPercentDiscounts.discountItemID, itemPercentDiscounts.discountItemName, itemPercentDiscounts.discountValue] = getItemPercentDiscount(ordersInfo.orderDetailArray, 0.5);
+
+  return priceBreakDiscount.discountValue >= itemPercentDiscounts.discountValue ? priceBreakDiscount : itemPercentDiscounts;
+}
+
+
+function getPriceBreakDiscountValue(orderPrice, price, discount) {
+  return Math.floor(orderPrice / price) * discount;
+}
+
+function getItemPercentDiscount(orderDetailArray, discountPercent) {
+  let discountItems = loadPromotions()[1].items;
+  let discountItemID = [];
+  let discountItemName = [];
+  let discountValue = 0;
+
+  for (let orderDetail of orderDetailArray) {
+    if (discountItems.includes(orderDetail.id)) {
+      discountItemID.push(orderDetail.id);
+      discountItemName.push(orderDetail.name);
+      discountValue += orderDetail.orderPrice * discountPercent;
+    }
+  }
+
+  return [discountItemID, discountItemName, discountValue];
+}
+
+function generateResult(ordersInfo) {
+  let result = '';
+  if (ordersInfo.discount.discountValue === 0) {
+    result = generateNoDiscountResult(ordersInfo);
+  }
+  else if (ordersInfo.discount.type === loadPromotions()[1].type) {
+    result = generateItemPercentDiscountsResult(ordersInfo);
+  }
+  else {
+    result = generatePriceBreakDiscountResult(ordersInfo);
+  }
+  return result;
+}
+
+function generatePriceBreakDiscountResult(ordersInfo) {
+  let detail = generateResultOrderDetail(ordersInfo.orderDetailArray);
+  let result = `
+============= 订餐明细 =============
+${detail}
+-----------------------------------
+使用优惠:
+${ordersInfo.discount.type}，省${ordersInfo.discount.discountValue}元
+-----------------------------------
+总计：${ordersInfo.totalPrice}元
+===================================`.trim();
+  return result;
+}
+
+function generateItemPercentDiscountsResult(ordersInfo) {
+  let detail = generateResultOrderDetail(ordersInfo.orderDetailArray);
+  let result = `
+============= 订餐明细 =============
+${detail}
+-----------------------------------
+使用优惠:
+${ordersInfo.discount.type}(${ordersInfo.discount.discountItemName.join('，')})，省${ordersInfo.discount.discountValue}元
+-----------------------------------
+总计：${ordersInfo.totalPrice}元
+===================================`.trim();
+  return result;
+}
+
+function generateNoDiscountResult(ordersInfo) {
+  let detail = generateResultOrderDetail(ordersInfo.orderDetailArray);
+  let result = `
+============= 订餐明细 =============
+${detail}
+-----------------------------------
+总计：${ordersInfo.totalPrice}元
+===================================`.trim();
+  return result;
+}
+
+function generateResultOrderDetail(orderDetailArray) {
+  return orderDetailArray.reduce((acc, cur) => acc + `${cur.name} x ${cur.amount} = ${cur.orderPrice}元
+`, '').trim();
 }
